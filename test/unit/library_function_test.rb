1e295442efa6091e23b88847895e5b3dcb2135da
Andre Quina <aquina@mitre.org>
2012-05-15 10:57:15 -0400

    added initial test for patient api extensions

--- test/unit/library_function_test.rb
+++ test/unit/library_function_test.rb
@@ -10,9 +10,9 @@ class LibraryFunctionTest < Test::Unit::TestCase
 
   def test_library_function_parses
     @context.eval('hQuery == undefined').must_equal false
-    @context.eval('hQuery.Patient == undefined').must_equal false
-    @context.eval('allTrue == undefined').must_equal false
-    @context.eval('atLeastOneTrue == undefined').must_equal false
+    @context.eval('typeof hQuery.Patient').must_equal "function"
+    @context.eval('typeof allTrue').must_equal "function"
+    @context.eval('typeof atLeastOneTrue').must_equal "function"
     
   end
   
@@ -26,6 +26,17 @@ class LibraryFunctionTest < Test::Unit::TestCase
     @context.eval('atLeastOneTrue(true,false,false)').must_equal true
     @context.eval('atLeastOneTrue(true,true,true)').must_equal true
     @context.eval('atLeastOneTrue(false,false,false)').must_equal false
+  end
+  
+  def test_patient_extensions
+    @context.eval('typeof hQuery.Patient.prototype.procedureResults').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.laboratoryTests').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.allMedications').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.allProblems').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.allDevices').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.activeDiagnoses').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.inactiveDiagnoses').must_equal "function"
+    @context.eval('typeof hQuery.Patient.prototype.resolvedDiagnoses').must_equal "function"
   end
 
 
