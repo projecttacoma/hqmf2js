a1cac1b35d24951baa34e29eb5cf943c4700bcd8
Andre Quina <andre.quina@gmail.com>
2013-01-14 14:05:00 -0500

    added filtering for outliers for ADE custom calculations based on measure definition

--- app/assets/javascripts/custom_calculations.js.coffee
+++ app/assets/javascripts/custom_calculations.js.coffee
@@ -1,19 +1,74 @@
 @hqmf.CustomCalc = {}
 
-@hqmf.CustomCalc.ADE_TTR_OBSERV = (patient) ->
-  []
+@hqmf.CustomCalc.ADE_TTR_OBSERV = (patient, hqmfjs) ->
+  inrReadings = DURING(hqmfjs.LaboratoryTestResultInr(patient), hqmfjs.MeasurePeriod(patient));
+  inrReadings = new hqmf.CustomCalc.PercentTTREntries(inrReadings)
+  [inrReadings.calculatePercentTTR()]
 
 class @hqmf.CustomCalc.PercentTTREntries extends hQuery.CodedEntryList
 
   constructor: (events) ->
     super()
-    events = events.sort(dateSortAscending)
-    @push(event) for event in events
     @minInr = 2.0
     @maxInr = 3.0
-    # sort entries ascending
-    # filter duplicates to those closest to 2.5
-    # remove duplicate results on entries
+    @minOutOfRange = 0.8
+    @maxOutOfRange = 10
+    @closestSetpoint = 2.5
+
+    clonedEvents = []
+    clonedEvents.push(new event.constructor(event.json)) for event in events
+
+    # remove entries < 0.8
+    # reset > 10 to be 10
+    # remove duplicate results on entries to those closest to 2.5
+    for entry in clonedEvents
+      currentClosestValue = null
+      for value in entry.values()
+        if value.scalar() > @maxOutOfRange
+          value.json['scalar'] = '10.0'
+        if value.scalar() >= @minOutOfRange && value.scalar() <= @maxOutOfRange
+          currentClosestValue = @closestValueToSetpoint(currentClosestValue, value)
+      
+      passingValues = []
+      passingValues = [currentClosestValue.json] if currentClosestValue?
+      entry.json['values'] = passingValues
+
+    # filter duplicates to those closest to 2.5 on the same day
+    # remove any entries with no values (removed because the value was below 0.8, or no value on source data)
+    entriesByDay = {}
+    for entry in clonedEvents
+      date = entry.timeStamp()
+      key = "#{date.getUTCFullYear()}_#{date.getUTCMonth()}_#{date.getUTCDate()}"
+      entriesByDay[key] = [] unless entriesByDay[key]
+      entriesByDay[key].push(entry) if entry.values().length > 0
+
+    # keep the closest entry to 2.5 when there are multiple entries per day
+    finalEvents = []
+    for key in _.keys(entriesByDay)
+      if (entriesByDay[key].length > 1)
+        currentClosestValue = null
+        selectedEntry = null
+        for entry in entriesByDay[key]
+          currentClosestValue = @closestValueToSetpoint(currentClosestValue, entry.values()[0])
+          if currentClosestValue.scalar() == entry.values()[0].scalar()
+            selectedEntry = entry
+        finalEvents.push(selectedEntry)
+      else
+        finalEvents = finalEvents.concat(entriesByDay[key])
+
+    finalEvents = finalEvents.sort(dateSortAscending)
+
+    @push(event) for event in finalEvents
+
+
+  closestValueToSetpoint: (one, two) ->
+    return two if one == null
+    return one if two == null
+    if (Math.abs(one.scalar() - @closestSetpoint) > Math.abs(two.scalar() - @closestSetpoint))
+      return two
+    else
+      return one
+
 
   calculateDaysInRange: (firstInr, secondInr) ->
 
--- test/unit/custom_calculations_test.rb
+++ test/unit/custom_calculations_test.rb
@@ -34,6 +34,42 @@ class CustomCalculationsTest < Test::Unit::TestCase
       inr_b9 = new hQuery.CodedEntry({time:#{Time.gm(2010,10,31).to_i}, values:[{scalar:'1.8'}]})
       
       list2 = new hqmf.CustomCalc.PercentTTREntries([inr_b1,inr_b2,inr_b3,inr_b4,inr_b5,inr_b6,inr_b7,inr_b8,inr_b9])
+
+      inr_c1 = new hQuery.CodedEntry({time:#{Time.gm(2010,1,5).to_i}, values:[{scalar:'2.8'}]})
+      inr_c2 = new hQuery.CodedEntry({time:#{Time.gm(2010,1,18).to_i}, values:[{scalar:'3.5'}]})
+      inr_c3 = new hQuery.CodedEntry({time:#{Time.gm(2010,2,2).to_i}, values:[{scalar:'3.4'}]})
+      inr_c4 = new hQuery.CodedEntry({time:#{Time.gm(2010,2,15).to_i}, values:[{scalar:'3.9'}]})
+      inr_c5 = new hQuery.CodedEntry({time:#{Time.gm(2010,3,10).to_i}, values:[{scalar:'1.7'}]})
+      inr_c6 = new hQuery.CodedEntry({time:#{Time.gm(2010,3,24).to_i}, values:[{scalar:'2.3'}]})
+      inr_c7 = new hQuery.CodedEntry({time:#{Time.gm(2010,4,12).to_i}, values:[{scalar:'2.4'}]})
+      inr_c8 = new hQuery.CodedEntry({time:#{Time.gm(2010,5,13).to_i}, values:[{scalar:'3.2'}]})
+      inr_c9 = new hQuery.CodedEntry({time:#{Time.gm(2010,5,27).to_i}, values:[{scalar:'3.5'}]})
+      inr_c10 = new hQuery.CodedEntry({time:#{Time.gm(2010,6,10).to_i}, values:[{scalar:'3.5'}]})
+      inr_c11 = new hQuery.CodedEntry({time:#{Time.gm(2010,6,24).to_i}, values:[{scalar:'3.4'}]})
+      inr_c12 = new hQuery.CodedEntry({time:#{Time.gm(2010,7,8).to_i}, values:[{scalar:'2.1'}]})
+      inr_c13 = new hQuery.CodedEntry({time:#{Time.gm(2010,7,22).to_i}, values:[{scalar:'2.6'}]})
+      
+      list3 = new hqmf.CustomCalc.PercentTTREntries([inr_c10,inr_c2,inr_c6,inr_c9,inr_c5,inr_c3,inr_c7,inr_c8,inr_c4,inr_c1,inr_c11,inr_c12,inr_c13])
+
+      inr_c1 = new hQuery.CodedEntry({time:#{Time.gm(2010,1,5).to_i}, values:[{scalar:'2.8'}]})
+      inr_c2 = new hQuery.CodedEntry({time:#{Time.gm(2010,1,18).to_i}, values:[{scalar:'3.5'}]})
+      inr_c3 = new hQuery.CodedEntry({time:#{Time.gm(2010,2,2).to_i}, values:[{scalar:'3.4'}]})
+      inr_c4 = new hQuery.CodedEntry({time:#{Time.gm(2010,2,2).to_i}, values:[{scalar:'2.1'}]})
+      inr_c5 = new hQuery.CodedEntry({time:#{Time.gm(2010,2,15).to_i}, values:[{scalar:'3.9'}]})
+      inr_c6 = new hQuery.CodedEntry({time:#{Time.gm(2010,3,10).to_i}, values:[{scalar:'1.7'}]})
+      inr_c7 = new hQuery.CodedEntry({time:#{Time.gm(2010,3,11).to_i}, values:[{scalar:'0.7'}]})
+      inr_c8 = new hQuery.CodedEntry({time:#{Time.gm(2010,3,24).to_i}, values:[{scalar:'2.9'},{scalar:'2.3'}]})
+      inr_c9 = new hQuery.CodedEntry({time:#{Time.gm(2010,4,12).to_i}, values:[{scalar:'2.4'}]})
+      inr_c10 = new hQuery.CodedEntry({time:#{Time.gm(2010,5,13).to_i}, values:[{scalar:'3.2'}]})
+      inr_c11 = new hQuery.CodedEntry({time:#{Time.gm(2010,5,13).to_i}, values:[{scalar:'3.6'}]})
+      inr_c12 = new hQuery.CodedEntry({time:#{Time.gm(2010,5,27).to_i}, values:[{scalar:'13.5'}]})
+      inr_c13 = new hQuery.CodedEntry({time:#{Time.gm(2010,6,10).to_i}, values:[{scalar:'3.5'}]})
+      inr_c14 = new hQuery.CodedEntry({time:#{Time.gm(2010,6,24).to_i}, values:[{scalar:'3.4'}]})
+      inr_c15 = new hQuery.CodedEntry({time:#{Time.gm(2010,7,8).to_i}, values:[{scalar:'12.1'}]})
+      inr_c16 = new hQuery.CodedEntry({time:#{Time.gm(2010,7,22).to_i}, values:[{scalar:'2.6'}, {scalar:'2.1'}]})
+      
+      list4 = new hqmf.CustomCalc.PercentTTREntries([inr_c9,inr_c8,inr_c3,inr_c14,inr_c10,inr_c6,inr_c7,inr_c2,inr_c1,inr_c5,inr_c11,inr_c12,inr_c13,inr_c4,inr_c15,inr_c16])
+
     "
     @context.eval(test_initialize_js)
     
@@ -69,6 +105,36 @@ class CustomCalculationsTest < Test::Unit::TestCase
   def test_calculate_percent_ttr
     assert @context.eval("Math.abs(list.calculatePercentTTR() - 43.99552468) < .001")
   end
+
+  def test_calculate_ttr_testdeck_record
+    assert @context.eval("Math.abs(list2.calculateTTR() - 203.66666666) < .001")
+  end
+
+  def test_calculate_percent_ttr_testdeck_record
+    assert @context.eval("Math.abs(list2.calculatePercentTTR() - 95.6181533646322) < .001")
+  end
   
+  def test_calculate_ttr_out_of_order
+    assert @context.eval("Math.abs(list3.calculateTTR() - 87.11113886) < .001")
+  end
+
+  def test_calculate_percent_ttr_out_of_order
+    assert @context.eval("Math.abs(list3.calculatePercentTTR() - 43.99552468) < .001")
+  end
+
+  def test_cleanup_of_inr_values
+
+    cleaned_inrs = [2.8, 3.5, 2.1, 3.9, 1.7, 2.3, 2.4, 3.2, 10.0, 3.5, 3.4, 10.0, 2.6]
+
+    assert @context.eval("list4.length == #{cleaned_inrs.size}")
+    cleaned_inrs.each_with_index do |inr,index|
+      assert @context.eval("list4[#{index}].values()[0].scalar() == #{inr}")
+    end
+
+    assert @context.eval("Math.abs(list4.calculateTTR() - 80.3184450684) < .001")
+    assert @context.eval("Math.abs(list4.calculatePercentTTR() - 40.56487124668) < .001")
+
+  end
+
   
 end
