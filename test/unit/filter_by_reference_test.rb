d46eb5b97ec9bfba183627f00782aa4952d9acc3
Rob Dingwell <bobd@mitre.org>
2015-01-13 15:33:19 -0500

    Adding test for filterEventsByReference

---
+++ test/unit/filter_by_reference_test.rb
@@ -0,0 +1,28 @@
+require_relative '../test_helper'
+require 'hquery-patient-api'
+
+class FilterByReferenceTest < Test::Unit::TestCase
+  
+  def setup
+    @context = get_js_context(HQMF2JS::Generator::JS.library_functions)
+  end
+
+  def test_filter
+    @context.eval "var evts = [new hQuery.CodedEntry({'_id' : 'id1', 'references' : [{'referenced_id' : 'id4', 'type' : 'fulfills'}]}),
+    new hQuery.CodedEntry({'_id' : 'id2','references' : [{'referenced_id' : 'id5', 'type' : 'some_reference'}]}),
+    new hQuery.CodedEntry({'_id' : 'id3'})]
+
+    var pos = [new hQuery.CodedEntry({'_id' : 'id4'}),
+    new hQuery.CodedEntry({'_id' : 'id5'}),
+    new hQuery.CodedEntry({'_id' : 'id6'})] 
+
+    "
+
+    assert_equal 1, @context.eval("filterEventsByReference(evts,'fulfills',pos).length")
+    assert_equal 'id1' , @context.eval("filterEventsByReference(evts,'fulfills',pos)[0].id")
+    assert_equal 1, @context.eval("filterEventsByReference(evts,'some_reference',pos).length")
+    assert_equal 'id2' , @context.eval("filterEventsByReference(evts,'some_reference',pos)[0].id")
+    assert_equal 0, @context.eval("filterEventsByReference(evts,'nonexistent_ref',pos).length")
+  end
+
+end
